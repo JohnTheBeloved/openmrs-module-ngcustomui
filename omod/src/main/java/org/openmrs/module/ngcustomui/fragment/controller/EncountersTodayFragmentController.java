e5da5c2c2fb79ebc89ccb1026474ca35a767fa61
John Alade <john.alade@interswitchng.com>
2015-03-23 01:58:38 +0000

    Using Ajax in a Fragment

--- omod/src/main/java/org/openmrs/module/ngcustomui/fragment/controller/EncountersTodayFragmentController.java
+++ omod/src/main/java/org/openmrs/module/ngcustomui/fragment/controller/EncountersTodayFragmentController.java
@@ -1,11 +1,14 @@
 package org.openmrs.module.ngcustomui.fragment.controller;
- 
- 
+  
 import java.util.Calendar;
 import java.util.Date;
- 
+import java.util.List;
+
+import org.openmrs.Encounter; 
 import org.openmrs.api.EncounterService;
 import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.openmrs.ui.framework.annotation.FragmentParam;
  
@@ -42,6 +45,25 @@ public class EncountersTodayFragmentController {
         endDate = defaultEndDate(startDate);
  
         model.addAttribute("encounters", service.getEncounters(null, null, startDate, endDate, null, null, null, false));
+    }
+
+    public List<SimpleObject> getEncounters(@RequestParam(value="start", required=false) Date startDate,
+                                            @RequestParam(value="end", required=false) Date endDate,
+                                            @RequestParam(value="properties", required=false) String[] properties,
+                                            @SpringBean("encounterService") EncounterService service,
+                                            UiUtils ui) {
+ 
+        if (startDate == null)
+            startDate = defaultStartDate();
+        if (endDate == null)
+            endDate = defaultEndDate(startDate);
+ 
+        if (properties == null) {
+            properties = new String[] { "encounterType", "encounterDatetime", "location", "provider" };
+        }
+ 
+        List<Encounter> encs = service.getEncounters(null, null, startDate, endDate, null, null, null, false);
+        return SimpleObject.fromCollection(encs, ui, properties);
     }
  
 }
